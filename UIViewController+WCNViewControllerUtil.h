a52b4256ad8abf4a93020005fe482dcf5cd60967
winforsky <1098412710@qq.com>
2015-02-09 14:40:18 +0800

    Create UIViewController+WCNViewControllerUtil.h
    /**
     *  在当前ViewController上弹出透明的ViewController
     */

---
+++ UIViewController+WCNViewControllerUtil.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (WCNViewControllerUtil)
+/**
+ *  在当前ViewController上弹出透明的ViewController
+ */
+- (void)toProVC:(UIViewController*)propVC;
+
+@end
